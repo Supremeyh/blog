6f28d71c619d89392b3489ba304bc4e876152cb0
supremeyh <supremeyh@126.com>
2018-11-27 09:44:15 +0800

    cors

--- snippets/http/server2.js
+++ snippets/http/server2.js
@@ -5,7 +5,10 @@ http.createServer(function(request, response){
   console.log('request come from ', request.url)
 
   response.writeHead(200, {
-    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888/'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Test-Cors',
+    'Access-Control-Allow-Methods': 'POST, PUT, DELETE',
+    'Access-Control-Max-Age': '86400' // 该响应的有效时间为86400秒,在有效时间内，浏览器无须为同一请求再次发起预检请求。
   })
 
   response.end('123')
