c2ea324471255fee8fa8f14b4eec0e48eb006570
supremeyh <supremeyh@126.com>
2018-12-20 21:40:17 +0800

    策略模式

--- segment/设计模式.js
+++ segment/设计模式.js
@@ -1,6 +1,8 @@
 // 设计模式 Design pattern, 开发过程中面临的一般问题的解决方案, 代表了最佳的实践
 
-// 单例模式: 保证一个类仅有一个实例，并提供访问此实例的全局访问点
+// https://godbmw.com/passages/2018-10-25-stragegy-pattern/
+
+// 单例模式 singleton pattern: 保证一个类仅有一个实例，并提供访问此实例的全局访问点
 // 用途:如果一个类负责连接数据库的线程池、日志记录逻辑等等，此时需要单例模式来保证对象不被重复创建，以达到降低开销的目的。
 
 const Singleton = function() {};
@@ -21,3 +23,27 @@ let s1 = Singleton.getInstance();
 let s2 = Singleton.getInstance();
 
 console.log(s1 === s2);
+
+
+// 策略模式 strategy pattern： 把一系列“可互换的”算法封装起来，并根据用户需求来选择其中一种。
+// 将算法的使用和算法的实现分离。算法的实现交给策略类。算法的使用交给环境类，环境类会根据不同的情况选择合适的算法。
+// 策略类
+const strategies = {
+  A() {
+    console.log("This is stragegy A");
+  },
+  B() {
+    console.log("This is stragegy B");
+  }
+};
+// 环境类
+const context = name => {
+  return strategies[name]();
+};
+
+// 调用策略A
+context("A");
+// 调用策略B
+context("B");
+
+
