8306fb5236531eaf54cb8e0e04db8b520c056648
supremeyh <supremeyh@126.com>
2018-12-20 22:20:14 +0800

    组合模式

--- segment/设计模式.js
+++ segment/设计模式.js
@@ -214,3 +214,71 @@ let refreshMenuBarCommand = RefreshMenuBarCommand(MenuBar);
 let button = document.querySelector("button");
 setCommand(button, refreshMenuBarCommand);
 
+
+
+// 组合模式： 将对象组合成树形结构以表示“部分-整体”的层次结构。
+// 用小的子对象构造更大的父对象，而这些子对象也由更小的子对象构成； 单个对象和组合对象对于用户暴露的接口具有一致性，而同种接口不同表现形式亦体现了多态性
+// 应用场景： 在需要针对“树形结构”进行操作的应用中使用，例如扫描文件夹、渲染网站导航结构等等。
+// 文件类
+class File {
+  constructor(name) {
+    this.name = name || "File";
+  }
+
+  add() {
+    throw new Error("文件夹下面不能添加文件");
+  }
+
+  scan() {
+    console.log("扫描文件: " + this.name);
+  }
+}
+
+// 文件夹类
+class Folder {
+  constructor(name) {
+    this.name = name || "Folder";
+    this.files = [];
+  }
+
+  add(file) {
+    this.files.push(file);
+  }
+
+  scan() {
+    console.log("扫描文件夹: " + this.name);
+    for (let file of this.files) {
+      file.scan();
+    }
+  }
+}
+
+let home = new Folder("用户根目录");
+
+let folder1 = new Folder("第一个文件夹"),
+  folder2 = new Folder("第二个文件夹");
+
+let file1 = new File("1号文件"),
+  file2 = new File("2号文件"),
+  file3 = new File("3号文件");
+
+// 将文件添加到对应文件夹中
+folder1.add(file1);
+
+folder2.add(file2);
+folder2.add(file3);
+
+// 将文件夹添加到更高级的目录文件夹中
+home.add(folder1);
+home.add(folder2);
+
+// 扫描目录文件夹
+home.scan();
+
+// 最终输出结果是：
+// 扫描文件夹: 用户根目录
+// 扫描文件夹: 第一个文件夹
+// 扫描文件: 1号文件
+// 扫描文件夹: 第二个文件夹
+// 扫描文件: 2号文件
+// 扫描文件: 3号文件
